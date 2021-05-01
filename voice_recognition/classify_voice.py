bc60241747ca750f40bcf6c824847824ffa75f6d
adargut <adar276@gmail.com>
2021-05-02 00:59:59 +0300

    somewhat done

--- voice_recognition/classify_voice.py
+++ voice_recognition/classify_voice.py
@@ -1,14 +1,7 @@
-# from __future__ import absolute_import
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.utils import shuffle
 import librosa
-import librosa.display as disp1
 import os
-import matplotlib.pyplot as plt
 import numpy as np
-import IPython.display as disp
 from sklearn.neighbors import KNeighborsClassifier
-import tqdm
 from sklearn.model_selection import train_test_split
 import argparse
 from sklearn.svm import SVC
@@ -16,12 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 import threading
-import logging
-import signal
-import sys
 import time
 import tqdm
 
@@ -35,55 +23,50 @@ import tqdm
 # TODO make data processing multi threaded
 
 
-MAX_TONNETZ_WEIGHT = 800
+MAX_TONNETZ_WEIGHT = 450
 MAX_SPECTOGRAM_WEIGHT = 1000
 MAX_TEMPOGRAM_WEIGHT = 300
 MAX_CHROMA_WEIGHT = 400
-MAX_MFCC_WEIGHT = 500  # most frequently used frequencies
-MAX_PITCH_WEIGHT = 500  # height of vocal tone
-MAX_SPEC_ROLLOF_WEIGHT = 5  # right skewedness of the waveform
-
-do_process = True
+MAX_MFCC_WEIGHT = 500
+MAX_PITCH_WEIGHT = 500
 
 
 class DatasetLoader:
-    def signal_handler(self, signal, frame):
-        global do_process
-        do_process = False
-        print('clicked ctrl+C')
-
-    def __init__(self, num_threads=5, max_files_to_process=15):
+    def __init__(self, num_threads=4, max_files_to_process_per_speaker=15, min_audio_file_duration=8, max_numbers_of_speakers=5):
         self.X = []
         self.Y = []
         self.threads = list()
         self.file_paths_to_process = list()
         self.num_threads = num_threads
         self.lock = threading.Lock()
-        self.do_process = True
-        self.max_files_to_process = max_files_to_process
+        self.max_files_to_process_per_speaker = max_files_to_process_per_speaker
         self.progress = None
+        self.min_audio_file_duration = min_audio_file_duration
+        self.max_numbers_of_speakers = max_numbers_of_speakers
 
     def load_dataset(self, path):
         for dir in os.listdir(path):
             processed_count = 0
-            if processed_count > self.max_files_to_process:
+            if processed_count > self.max_files_to_process_per_speaker:
                 continue
-            if int(dir[2:]) > 10005:
+            if int(dir[2:]) > 10000 + self.max_numbers_of_speakers:
                 return
             if os.path.isdir(path + '/' + dir):
                 for inner_dir in os.listdir(path + '/' + dir):
-                    if processed_count > self.max_files_to_process:
+                    if processed_count > self.max_files_to_process_per_speaker:
                         break
                     for audio_file_to_process in os.listdir(path + '/' + dir + '/' + inner_dir):
                         full_path = path + '/' + dir + '/' + inner_dir
+                        if librosa.get_duration(librosa.load(full_path + '/' + audio_file_to_process, sr=None)[0]) < self.min_audio_file_duration:
+                            continue
                         self.file_paths_to_process.append((full_path, audio_file_to_process,))
                         processed_count += 1
-                        if processed_count > self.max_files_to_process:
+                        if processed_count > self.max_files_to_process_per_speaker:
                             break
 
     def process_dataset(self):
         split = np.array_split(self.file_paths_to_process, self.num_threads)
-        signal.signal(signal.SIGINT, self.signal_handler)
+        print('processing dataset with {0} threads and {1} total audio files'.format(self.num_threads, len(self.file_paths_to_process)))
         self.progress = tqdm.tqdm(total=len(self.file_paths_to_process), desc='Extracting features from audio')
 
         with self.progress:
@@ -107,9 +90,6 @@ class DatasetLoader:
             self.process_audio_file(filepath, filename)
 
     def process_audio_file(self, filepath, filename: str):
-        if not do_process:
-            return
-        # print('processing file', filepath + '/' + filename, 'from thread', threading.get_ident())
         label = filepath.split('/')[-2]
         features = self.extract_features(filepath, filename)
         self.lock.acquire()
@@ -122,19 +102,17 @@ class DatasetLoader:
         full_path = filepath + '/' + filename
         if not os.path.isfile(full_path):
             raise Exception('invalid path: ' + full_path)
-        samples, sample_rate = librosa.load(full_path)
+        samples, sample_rate = librosa.load(full_path, sr=None, duration=5)
         tonnetz = librosa.feature.tonnetz(y=samples, sr=sample_rate)
         chroma_stft = librosa.feature.chroma_stft(y=samples, sr=sample_rate)
         tempogram = librosa.feature.tempogram(y=samples, sr=sample_rate)
         spectogram = librosa.feature.melspectrogram(samples, sample_rate)
         mfcc = librosa.feature.mfcc(y=samples, sr=sample_rate)
-        spec_rolloff = librosa.feature.spectral_rolloff(y=samples, sr=sample_rate)
         pitches, magnitudes = librosa.piptrack(y=samples, sr=sample_rate)
         return np.concatenate([tonnetz.flatten()[:MAX_TONNETZ_WEIGHT], spectogram.flatten()[:MAX_SPECTOGRAM_WEIGHT],
                                tempogram.flatten()[:MAX_TEMPOGRAM_WEIGHT],
                                chroma_stft.flatten()[:MAX_CHROMA_WEIGHT],
-                               mfcc.flatten()[:MAX_MFCC_WEIGHT], spec_rolloff.flatten()[:MAX_SPEC_ROLLOF_WEIGHT],
-                               magnitudes.flatten()[:MAX_PITCH_WEIGHT]])
+                               mfcc.flatten()[:MAX_MFCC_WEIGHT], magnitudes.flatten()[:MAX_PITCH_WEIGHT]])
 
 
 def test_accuracy(X, Y, classifier: KNeighborsClassifier):
@@ -151,7 +129,7 @@ def test_accuracy(X, Y, classifier: KNeighborsClassifier):
 def get_classifier(classifier):
     print('classifier:', classifier)
     if classifier == 'knn':
-        return KNeighborsClassifier(n_neighbors=1)
+        return KNeighborsClassifier(n_neighbors=3)
     if classifier == 'linear_svm':
         return SVC(kernel='linear')
     if classifier == 'poly_svm':
@@ -161,7 +139,7 @@ def get_classifier(classifier):
     if classifier == 'dt':
         return DecisionTreeClassifier()
     if classifier == 'nn':
-        return MLPClassifier()
+        return MLPClassifier(hidden_layer_sizes=(500,), alpha=0.1, learning_rate_init=0.1)
     if classifier == 'bayes':
         return GaussianNB()
     if classifier == 'ada':
